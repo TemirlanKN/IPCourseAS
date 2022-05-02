f4e7beac008a25421b65b6cf9c0e4ab535056b2e
TemirlanKN <temirlan9908@gmail.com>
2022-05-02 02:46:15 -0700

    Buttons adjusted with Toast/AlertDialog

--- app/src/main/java/com/trasty/itprogercourse/MainActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/MainActivity.java
@@ -1,37 +1,57 @@
 package com.trasty.itprogercourse;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView resultTextView;
-    private EditText number_field_1, number_field_2;
-    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById(R.id.res_text);
-        number_field_1 = findViewById(R.id.number_field_1);
-        number_field_2 = findViewById(R.id.number_field_2);
-        addButton = findViewById(R.id.add_button);
-
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float num1 = Float.parseFloat(number_field_1.getText().toString());
-                float num2 = Float.parseFloat(number_field_2.getText().toString());
-                float res = num1 + num2;
-                resultTextView.setText(String.valueOf(res));
-            }
+        Button button2 = findViewById(R.id.button2);
+        TextView mainText = findViewById(R.id.mainText);
+        button2.setOnClickListener(view -> {
+//                showInfo(mainText.getText().toString(), button2);
+            showInfoAlert("Do you want to exit?");
         });
+
+    }
+
+    public void buttonClick(View v){
+        showInfo(((Button) v).getText().toString(), ((Button) v));
+    }
+
+    private void showInfo(String text, Button button){
+        button.setText(R.string.ClickedText);
+        button.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showInfoAlert(String text){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Big hint")
+                .setMessage(text)
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialogInterface, i) -> finish())
+                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
+
+
+
+
+
