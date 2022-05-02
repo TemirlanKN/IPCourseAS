f3dd24b155bfcb57881b7ea7c0577cbfa70582df
TemirlanKN <temirlan9908@gmail.com>
2022-05-02 14:56:43 -0700

    Second activity/lava class

--- app/src/main/java/com/trasty/itprogercourse/MainActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,35 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button2 = findViewById(R.id.button2);
-        TextView mainText = findViewById(R.id.mainText);
-        button2.setOnClickListener(view -> {
-//                showInfo(mainText.getText().toString(), button2);
-            showInfoAlert("Do you want to exit?");
-        });
-
     }
 
-    public void buttonClick(View v){
-        showInfo(((Button) v).getText().toString(), ((Button) v));
-    }
-
-    private void showInfo(String text, Button button){
-        button.setText(R.string.ClickedText);
-        button.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
-
-    private void showInfoAlert(String text){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Big hint")
-                .setMessage(text)
-                .setCancelable(false)
-                .setPositiveButton("Yes", (dialogInterface, i) -> finish())
-                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel());
-        AlertDialog dialog = builder.create();
-        dialog.show();
+    public void startNewActivity(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
 
