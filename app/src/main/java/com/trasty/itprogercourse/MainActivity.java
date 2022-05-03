2c73320097fa8f7c01ce4b70746ef44c77601228
TemirlanKN <temirlan9908@gmail.com>
2022-05-03 04:24:12 -0700

    fragments 1 and 2

--- app/src/main/java/com/trasty/itprogercourse/MainActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/MainActivity.java
@@ -1,45 +1,40 @@
 package com.trasty.itprogercourse;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.trasty.itprogercourse.sounds.playSound;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton mortalCombat;
-    private MediaPlayer mkSound;
+    private FirstFragment firstFragment = new FirstFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mortalCombat = findViewById(R.id.mortalKombatImage);
+        Button btn_fragment1 = findViewById(R.id.btn_fragment1);
+        Button btn_fragment2 = findViewById(R.id.btn_fragment2);
 
-        mkSound = MediaPlayer.create(this, R.raw.mortal_kombat);
+        setNewFragment(firstFragment);
 
-        mortalCombat.setOnClickListener(view -> playSound.soundPlayButton(mkSound));
+        btn_fragment1.setOnClickListener(view -> setNewFragment(firstFragment));
+
+        btn_fragment2.setOnClickListener(view -> {
+                SecondFragment secondFragment = new SecondFragment();
+                setNewFragment(secondFragment);
+        });
     }
 
-    public void startNewActivity(View v){
-        Intent intent = new Intent(this, secondActivity.class);
-        playSound.soundStopButton(mkSound);
-        startActivity(intent);
+    private void setNewFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_layout, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
     }
-
 }
