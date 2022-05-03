e7ce5c97ce75f680a15d9f82dafa14b71d09b98c
TemirlanKN <temirlan9908@gmail.com>
2022-05-03 15:16:14 -0700

    Design app

--- app/src/main/java/com/trasty/itprogercourse/MainActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/MainActivity.java
@@ -11,30 +11,10 @@ import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirstFragment firstFragment = new FirstFragment();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btn_fragment1 = findViewById(R.id.btn_fragment1);
-        Button btn_fragment2 = findViewById(R.id.btn_fragment2);
-
-        setNewFragment(firstFragment);
-
-        btn_fragment1.setOnClickListener(view -> setNewFragment(firstFragment));
-
-        btn_fragment2.setOnClickListener(view -> {
-                SecondFragment secondFragment = new SecondFragment();
-                setNewFragment(secondFragment);
-        });
     }
 
-    private void setNewFragment(Fragment fragment) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.frame_layout, fragment);
-        ft.addToBackStack(null);
-        ft.commit();
-    }
 }
