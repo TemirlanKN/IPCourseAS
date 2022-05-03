b47f6f5daf4f2fcfb8e2507971b9299cf915ebb7
TemirlanKN <temirlan9908@gmail.com>
2022-05-02 17:33:37 -0700

    sounds and imageButtons

--- app/src/main/java/com/trasty/itprogercourse/MainActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/MainActivity.java
@@ -7,29 +7,39 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.trasty.itprogercourse.sounds.playSound;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mortalCombat;
+    private MediaPlayer mkSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mortalCombat = findViewById(R.id.mortalKombatImage);
+
+        mkSound = MediaPlayer.create(this, R.raw.mortal_kombat);
+
+        mortalCombat.setOnClickListener(view -> playSound.soundPlayButton(mkSound));
     }
 
     public void startNewActivity(View v){
         Intent intent = new Intent(this, secondActivity.class);
+        playSound.soundStopButton(mkSound);
         startActivity(intent);
     }
 
 }
-
-
-
-
-
--- app/src/main/java/com/trasty/itprogercourse/secondActivity.java
+++ app/src/main/java/com/trasty/itprogercourse/secondActivity.java
@@ -3,19 +3,34 @@ package com.trasty.itprogercourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+
+import com.trasty.itprogercourse.sounds.playSound;
 
 public class secondActivity extends AppCompatActivity {
+
+    private MediaPlayer SPsound;
+    private ImageButton spiderManButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        spiderManButton = findViewById(R.id.spiderManButton);
+
+        SPsound = MediaPlayer.create(this, R.raw.spiderman_3_black);
+
+        spiderManButton.setOnClickListener(view -> playSound.soundPlayButton(SPsound));
     }
 
     public void goBack(View v){
         Intent intent = new Intent(this, MainActivity.class);
+        playSound.soundStopButton(SPsound);
         startActivity(intent);
     }
+
 }
